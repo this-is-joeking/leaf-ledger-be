55d27b8673f9bbd72be523428fee0b530c73fde6
Joe King <j.king92@gmail.com>
2023-06-14 19:16:25 -0700

    feat: call plant data from database if it already exists

--- spec/requests/plants/show_spec.rb
+++ spec/requests/plants/show_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'plant details', :vcr do
     plant_name = 'Jalapeno'
     expected_keys = %i[scientific_name common_name sun_exposure planting_method planting_time watering
                        fertilization pruning harvest_method harvest_timeline pests homeopathic_remedies spacing other_notes].sort
+
+    expect(Plant.count).to eq(0)
     headers = {
       'CONTENT_TYPE' => 'application/json',
       'ACCEPT' => 'application/json'
@@ -13,6 +15,7 @@ RSpec.describe 'plant details', :vcr do
 
     plant_data = JSON.parse(response.body, symbolize_names: true)
 
+    expect(Plant.count).to eq(1)
     expect(response).to be_successful
     expect(plant_data).to be_a Hash
     expect(plant_data.keys).to eq([:data])
@@ -27,5 +30,38 @@ RSpec.describe 'plant details', :vcr do
       expect(plant_data[:data][:attributes][key]).to be_a String
     end
     expect(plant_data[:data][:attributes][:common_name]).to eq(plant_name)
+  end
+
+  it 'returns an existing plant in the database if it finds a case insensitive match for the plant name' do
+    plant = create(:plant, common_name: 'jalapeno')
+    expect(Plant.count).to eq(1)
+
+    plant_name = 'Jalapeno'
+    expected_keys = %i[scientific_name common_name sun_exposure planting_method planting_time watering
+                       fertilization pruning harvest_method harvest_timeline pests homeopathic_remedies spacing other_notes].sort
+
+    headers = {
+      'CONTENT_TYPE' => 'application/json',
+      'ACCEPT' => 'application/json'
+    }
+    get("/api/v1/plants/#{plant_name}", headers:)
+
+    plant_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(Plant.count).to eq(1)
+    expect(response).to be_successful
+    expect(plant_data).to be_a Hash
+    expect(plant_data.keys).to eq([:data])
+    expect(plant_data[:data]).to be_a Hash
+    expect(plant_data[:data].keys.sort).to eq(%i[type id attributes].sort)
+    expect(plant_data[:data][:id]).to be_a String
+    expect(plant_data[:data][:id].to_i).to be_a Integer
+    expect(plant_data[:data][:type]).to eq('plant')
+    expect(plant_data[:data][:attributes]).to be_a Hash
+    expect(plant_data[:data][:attributes].keys.sort).to eq(expected_keys)
+    expected_keys.each do |key|
+      expect(plant_data[:data][:attributes][key]).to be_a String
+    end
+    expect(plant_data[:data][:attributes][:common_name].downcase).to eq(plant_name.downcase)
   end
 end

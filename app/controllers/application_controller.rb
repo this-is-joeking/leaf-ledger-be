281322e42c2698fca6665d0edf2abedd397b4e41
Joe King <j.king92@gmail.com>
2023-07-05 16:53:05 -0700

    Add error handling for record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_error
+
+  private
+
+  def render_error(error)
+    render json: ErrorSerializer.not_found(error.message), status: :not_found
+  end
 end

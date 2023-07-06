b04daabe5736149b6e36dbda4bf7301c5cb75622
Joe King <j.king92@gmail.com>
2023-07-06 10:16:24 -0700

    Refresh cassettes

--- app/facades/plant_facade.rb
+++ app/facades/plant_facade.rb
@@ -5,7 +5,9 @@ class PlantFacade
     plant_data = PlantService.new(plant_name)
 
     raise PlantError, "Could not find any plant matching the name: #{plant_name}" if plant_data == 422
-
+    plant_img = ImageService.new(plant_name)[:data].first[:url]
+    plant_data[:plant_img] = plant_img
+    
     Plant.create!(plant_data)
   end
 end

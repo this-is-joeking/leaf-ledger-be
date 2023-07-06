f997ded66426d40169d5951c045c7640726d799c
Joe King <j.king92@gmail.com>
2023-07-06 10:51:18 -0700

    Add initial method of image attachment to plant model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,6 +3,13 @@
 class Plant < ApplicationRecord
   has_many :user_plants
   has_many :users, through: :user_plants
+  has_one_attached :ai_generated_image
+  before_save :grab_image
 
   validates_presence_of :scientific_name, :common_name
+
+  def grab_image
+    downloaded_image = Down.download(self.plant_img)
+    ai_generated_image.attach(io: downloaded_image, filename: "#{self.common_name}.jpg")
+  end
 end

de4dacdddf73eb166ff940cb73b97f3391afb3e8
Joe King <j.king92@gmail.com>
2023-07-07 09:29:15 -0700

    Add default host for dev and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  Rails.application.routes.default_url_options[:host] = ENV['BASE_URL']
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local

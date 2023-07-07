dfe23198257714f5d755c6427001ca59e1044321
Joe King <j.king92@gmail.com>
2023-07-07 09:39:42 -0700

    Add default host environments/test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
+  Rails.application.routes.default_url_options[:host] = ENV['BASE_URL']
+
 
   # Eager loading loads your whole application. When running a single test locally,
   # this probably isn't necessary. It's a good idea to do in a continuous integration

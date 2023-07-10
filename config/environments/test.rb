0ee3e3557f0ed07cd5f124629dc6832a056885fd
Joe King <j.king92@gmail.com>
2023-07-10 08:55:32 -0700

    Add asset host url

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,11 +9,12 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  Rails.application.routes.default_url_options[:host] = 'http://localhost:3000'
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
   
   config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+  config.action_mailer.asset_host = 'http://localhost:3000'
 
   # Eager loading loads your whole application. When running a single test locally,
   # this probably isn't necessary. It's a good idea to do in a continuous integration

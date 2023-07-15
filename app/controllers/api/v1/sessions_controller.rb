cdf22a3b7cb164b086c26f5f57812dc01ffbf9d1
Joe King <j.king92@gmail.com>
2023-07-15 13:31:40 -0700

    Update session controller with basic setup of Faraday connection for validating access token still need to split to service facade pattern

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,15 +2,33 @@ module Api
   module V1
     class SessionsController < ApplicationController
       def create
-        require 'pry'
-        binding.pry
+        conn = Faraday.new(
+          url: 'https://www.googleapis.com/oauth2/v3/tokeninfo', 
+          headers: {
+            Authorization: "Bearer #{params[:headers][:GoogleToken][:access_token]}",
+            Accept: 'application/json'
+          })
+        response = conn.get
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        
+        require 'pry'; binding.pry
+        
       end
 
       private
 
       def new_session_params
-        params.require(:session).permit(headers: [:Authorization])
+        # params.require(:session).permit(headers: [:Authorization])
       end
     end
   end
 end
+
+# This way works
+
+
+
+
+# then send conn.get and data is returned for user
+# still need to research ways to verify other parts of token instead
+# of assuming gogole is doing ll verifying

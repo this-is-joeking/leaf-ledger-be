6214daa3488131b05d41e843db110c24d04941ba
Joe King <j.king92@gmail.com>
2023-07-20 16:34:19 -0700

    test: Update user factory to generate random unique gid for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :user do
     name { Faker::Movies::Lebowski.character }
     email { Faker::Internet.email(name:) }
+    gid { Faker::Number.unique.number(digits: 20)}
   end
 end
